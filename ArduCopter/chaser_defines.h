c99515eca0a4b0cabc6df9c3891f2ea3557ebf1a
Kentaro <kntr.fkd.biz@gmail.com>
2014-03-28 22:33:04 +0900

    ver1.4_beta67: 池の平用ジオフェンス

--- ArduCopter/chaser_defines.h
+++ ArduCopter/chaser_defines.h
@@ -50,10 +50,10 @@
 
 // 受け取ったビーコン位置の緯度経度の上下限界を設定
 // 刈谷用（石浜駅とトヨタ自動車高岡工場を結ぶ直線を対辺とした長方形の枠内）
-#define CHASER_LAT_MIN            349682650			//経度下限
-#define CHASER_LAT_MAX            350551540			//緯度上限
-#define CHASER_LON_MIN            1369701330		//経度下限
-#define CHASER_LON_MAX            1370635160		//経度上限
+//#define CHASER_LAT_MIN            349682650			//経度下限
+//#define CHASER_LAT_MAX            350551540			//緯度上限
+//#define CHASER_LON_MIN            1369701330		//経度下限
+//#define CHASER_LON_MAX            1370635160		//経度上限
 
 // スノーウェーブパーク、鷲ヶ岳用
 //#define CHASER_LAT_MIN            358993800			//経度下限
@@ -62,10 +62,10 @@
 //#define CHASER_LON_MAX            1370295750		//経度上限
 
 // 池の平温泉スキー場用
-//#define CHASER_LAT_MIN            368389520			//経度下限
-//#define CHASER_LAT_MAX            368999260			//緯度上限
-//#define CHASER_LON_MIN            1381150330		//経度下限
-//#define CHASER_LON_MAX            1382204330		//経度上限
+#define CHASER_LAT_MIN            368389520			//経度下限
+#define CHASER_LAT_MAX            368999260			//緯度上限
+#define CHASER_LON_MIN            1381150330		//経度下限
+#define CHASER_LON_MAX            1382204330		//経度上限
 
 
 
